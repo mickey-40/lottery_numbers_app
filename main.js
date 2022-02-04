4428e0b69d9eff6cae962d32be3fd450a09de1b0
Mickey Arnold <mickeyarnold@Mickeys-MBP.lan>
2022-02-04 13:53:31 -0600

    added some code and working on function

---
+++ main.js
@@ -0,0 +1,31 @@
+//Picks numbers from 1-70
+
+
+let numArray1 = [];
+let numArray2 = [];
+  
+ for (i = 0; i < 5; i++){
+    let num = Math.floor(Math.random() * 70) + 1;
+    numArray1.push(num);
+} 
+let bonusBall1 = Math.floor(Math.random() * 25) + 1;
+
+for (j = 0; j < 5; j++){
+    let num = Math.floor(Math.random() * 69) + 1;
+    numArray2.push(num);
+} 
+let bonusBall2 = Math.floor(Math.random() * 26 + 1);
+
+
+
+
+
+
+      
+    
+    
+
+
+    
+   
+
