4c8074c98923357209a743f3c365846d098c6d0d
Mickey Arnold <mickeyarnold@Mickeys-MBP.lan>
2022-02-04 15:50:53 -0600

    Added a button and the functions are working

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 //Picks numbers from 1-70
-
-
+const powernum = function (){
 let numArray1 = [];
-let numArray2 = [];
-  
  for (i = 0; i < 5; i++){
     let num = Math.floor(Math.random() * 70) + 1;
     numArray1.push(num);
 } 
+document.getElementById("powernum").innerHTML = numArray1;
+}
+
+window.onload = function() {
+    powernum();
+    document.getElementById("generate").addEventListener('click', powernum);
+}
 let bonusBall1 = Math.floor(Math.random() * 25) + 1;
 
+
+let numArray2 = [];
 for (j = 0; j < 5; j++){
     let num = Math.floor(Math.random() * 69) + 1;
     numArray2.push(num);
