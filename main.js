61797db608d1fdadc8836eb6915d543e73877a00
mickey-40 <mickeyarnold53@yahoo.com>
2022-05-06 07:20:22 -0500

    Added dom function for both arrays to appear when button is pushed.

--- main.js
+++ main.js
@@ -1,37 +1,20 @@
-//Picks numbers from 1-70
-const powernum = function (){
-let numArray1 = [];
- for (i = 0; i < 5; i++){
-    let num = Math.floor(Math.random() * 70) + 1;
-    numArray1.push(num);
-} 
-document.getElementById("powernum").innerHTML = numArray1;
-}
 
-window.onload = function() {
-    powernum();
-    document.getElementById("generate").addEventListener('click', powernum);
-}
-let bonusBall1 = Math.floor(Math.random() * 25) + 1;
-
-
-let numArray2 = [];
-for (j = 0; j < 5; j++){
-    let num = Math.floor(Math.random() * 69) + 1;
-    numArray2.push(num);
-} 
-let bonusBall2 = Math.floor(Math.random() * 26 + 1);
-
-
-
-
-
-
-      
-    
-    
-
-
-    
-   
-
+    document.addEventListener('click', function (){
+    let numArray1 = [];
+    let numArray2 = [];
+     for (i = 1; i <= 6; i++){
+        if(i<6){
+          let num1 = Math.floor(Math.random() * 70) + 1;
+          let num2 = Math.floor(Math.random() * 70) + 1;
+          numArray1.push(num1);
+          numArray2.push(num2);
+    }else {
+      let num1 = Math.floor(Math.random() * 26) + 1;
+      let num2 = Math.floor(Math.random() * 25) + 1;
+      numArray1.push(num1);
+      numArray2.push(num2);
+    }
+    document.getElementById("powernum").innerHTML = numArray1;
+    document.getElementById("meganum").innerHTML = numArray2;
+    }
+    });
